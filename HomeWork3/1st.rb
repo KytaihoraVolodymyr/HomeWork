1964e85070d4a74b973f1ec39c9115b5fcd11068
Kytaihora Volodymyr <kytaihora.v@gmail.com>
2021-07-01 13:34:22 +0300

    Update def

--- HomeWork3/1st.rb
+++ HomeWork3/1st.rb
@@ -1,14 +1,17 @@
 puts "Введите количество элементов массива"
 kol = gets.chomp().to_i
-old = []
+old = Array.new
 
 for i in 1..kol
     puts"Введите возраст"
     old << gets.chomp().to_i
 end
-print old   
+print old  
 
-sorted_arr = old.sort
-new = []
-new << sorted_arr[-2] << sorted_arr[-1]
-print new
+def twoOldestAges old
+    new = Array.new 
+    sorted_arr = old.sort
+    new << sorted_arr[-2] << sorted_arr[-1]
+    print new
+end
+twoOldestAges(old)
